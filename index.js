2e23f0259b817eda96227e1cce535d1a69fa1f1e
Stein Astor Fernandez <saf6111@g.rit.edu>
2017-11-23 21:25:53 -0800

    serving webpage skeleton.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(req, res){
+    res.sendFile('default.html', { root: __dirname + "/www" } );
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
