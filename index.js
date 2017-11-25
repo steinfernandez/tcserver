64a4994c9147f138b57a5bdc27b53d1ca3bcfbcb
Stein Astor Fernandez <saf6111@g.rit.edu>
2017-11-24 22:44:41 -0800

    userstuff + moar api

--- index.js
+++ index.js
@@ -1,39 +1,109 @@
 const express = require('express')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const app = express()
-const mongotalk = require('./js/mongotalk.js');
+const session = require("express-session")
+const bodyParser = require("body-parser")
+const mongotalk = require('./js/mongotalk.js')
 
 const SUCCESS_OBJ = true;
 const FAILURE_OBJ = false;
+const UNAUTH_OBJ = "bad cat you can't do that";
 
-app.get('/', function(req, res) {
-    res.sendFile('default.html', { root: __dirname + "/www" } );
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
 
-app.get('/main.js', function(req, res) {
-    res.sendFile('main.js', { root: __dirname + "/www/js" } );
+passport.deserializeUser(function(id, done) {
+    done(null, id);
+});
+
+passport.use(new LocalStrategy(
+        function(username, password, done) {
+                mongotalk.user.auth(username, password, function (user) {
+                if (!user)
+                        return done(null, false, { message: 'Authentication failed.' });
+                else
+                        return done(null, user);
+                });
+        }
+));
+
+app.use(express.static("public"));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}))
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+function ensureAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) { return next(); }
+  res.redirect('/login')
+}
+
+app.post('/login',
+        passport.authenticate('local'),
+        function(req, res) {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.send(req.user);
 });
 
 app.post('/addcategory', function (req, res) {
-        try {
-                mongotalk.board.addCategory(req.username,req.newcat);
-                res.send(SUCCESS_OBJ)
-        } catch(err) {
-                res.send(FAILURE_OBJ)
-        };
+        if(!req.isAuthenticated())
+                res.send(UNAUTH_OBJ)
+        else
+        {
+                try {
+                        mongotalk.board.addCategory(req.user.username,JSON.parse(req.body.newcat));
+                        res.send(SUCCESS_OBJ)
+                } catch(err) {
+                        res.send(FAILURE_OBJ)
+                };
+        }
 });
 
 app.post('/removecategory', function (req, res) {
-        try {
-                mongotalk.board.removeCategory(req.username,req.delcat);
-                res.send(SUCCESS_OBJ)
-        } catch(err) {
-                res.send(FAILURE_OBJ)
-        };
+        if(!req.isAuthenticated())
+                res.send(UNAUTH_OBJ)
+        else
+        {
+                try {
+                        mongotalk.board.removeCategory(req.user.username, req.body.catindex);
+                        res.send(SUCCESS_OBJ)
+                } catch(err) {
+                        res.send(FAILURE_OBJ)
+                };
+        }
 });
 
 app.post('/updatecategory', function (req, res) {
+        if(!req.isAuthenticated())
+                res.send(UNAUTH_OBJ)
+        else
+        {
+                try {
+                        mongotalk.board.updateCategory(req.user.username, parseInt(req.body.catindex), JSON.parse(req.body.newcat));
+                        res.send(SUCCESS_OBJ)
+                } catch(err) {
+                        res.send(FAILURE_OBJ)
+                };
+        }
+});
+
+app.post('/create', function (req, res) {
         try {
-                mongotalk.board.updateCategory(req.username, req.oldcat, req.newcat);
+                mongotalk.user.create(req.body.username, req.body.password);
                 res.send(SUCCESS_OBJ)
         } catch(err) {
                 res.send(FAILURE_OBJ)
--- js/mongotalk.js
+++ js/mongotalk.js
@@ -1,17 +1,5 @@
 var mongoclient = require('mongodb').MongoClient;
 var uri = "mongodb://stein:steinstein@cluster0-shard-00-00-ywtuf.mongodb.net:27017,cluster0-shard-00-01-ywtuf.mongodb.net:27017,cluster0-shard-00-02-ywtuf.mongodb.net:27017/tcserver?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-mongoclient.connect(uri, function(err, db) {
-  // Paste the following examples here
-  if(err)
-        console.log(err);
-  //asdjfskdjfnskdjfnskjdfn
-//var cursor = db.collection('users').find({ username: "user1" }, function(err,result) { console.log(result.username); });
-db.collection("users").find({ username:"user1"}, { }).toArray(function(err, result) {
-    if (err) throw err;
-    console.log(result);
-    db.close();
-});
-});
 
 function boardObj()
 {
@@ -22,9 +10,8 @@ function boardObj()
 
 function userObj()
 {
-        this.signUp = signUp;
-        this.logIn = logIn;
-        this.logOut = logOut;
+        this.create = create;
+        this.auth = auth;
 }
 
 function addCategory(username,newcat)
@@ -45,7 +32,7 @@ function addCategory(username,newcat)
         });
 }
 
-function removeCategory(username,delcat)
+function removeCategory(username,catindex)
 {
         //authenticate user, then proceed
         mongoclient.connect(uri, function(err, db) {
@@ -53,7 +40,7 @@ function removeCategory(username,delcat)
                 db.collection("users").findOne({username:username}, function(err1, result1) {
                         if (err1) throw err1;
                         console.log("1 user found");
-                        result1.board = result1.board.filter(cat => cat.categoryName != delcat);
+                        result1.board.splice(catindex,1);
                         db.collection("users").updateOne({username:username}, result1 ,function(err2, result2) {
                                 if (err2) throw err2;
                                 console.log("1 user updated");
@@ -63,7 +50,7 @@ function removeCategory(username,delcat)
         });
 }
 
-function updateCategory(username,oldcat,newcat)
+function updateCategory(username,catindex,newcat)
 {
         //authenticate user, then proceed
         mongoclient.connect(uri, function(err, db) {
@@ -71,19 +58,50 @@ function updateCategory(username,oldcat,newcat)
                 db.collection("users").findOne({username:username}, function(err1, result1) {
                         if (err1) throw err1;
                         console.log("1 user found");
-                        Object.assign(result1.board.find(cat => cat.categoryName == oldcat), newcat);
+                        //Object.assign(result1.board.find(cat => cat.categoryName == oldcat), newcat);
+                        result1.board[catindex] = newcat;
                         db.collection("users").updateOne({username:username}, result1 ,function(err2, result2) {
                                 if (err2) throw err2;
                                 console.log("1 user updated");
                                 db.close();
                         });
+                });
+        });
+}
+
+function create(username,password)
+{
+        mongoclient.connect(uri, function(err, db) {
+                if (err) throw err;
+                var newuserobj = {username:username, password:password, board:[]};
+                db.collection("users").insertOne(newuserobj, function(err1, result1) {
+                        if (err1) throw err1;
+                        console.log("1 user created");
+                        db.close();
+                });
+        });
+}
+
+function auth(username,password,cb)
+{
+        mongoclient.connect(uri, function(err, db) {
+                if (err) throw err;
+                db.collection("users").findOne({username:username}, function(err1, result1) {
+                        if (err1) throw err1;
+                        console.log("1 user found");
+                        if(result1.password == password)
+                                cb(result1);
+                        else
+                                cb(null);
+                        db.close();
                 });
         });
 }
 
 var board = new boardObj();
+var user = new userObj();
 
-var mongotalk = { board:board };
+var mongotalk = { board:board, user:user };
 
 module.exports = mongotalk;
 
@@ -93,3 +111,5 @@ module.exports = mongotalk;
 //removeCategory("user1","bunnies");
 
 //updateCategory("user1","category1",newcat);
+
+//user.create("cakes","cakescakes");
